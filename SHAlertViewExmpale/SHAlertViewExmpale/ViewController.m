08f735e6a99aa8d31388948e7a91996a7aa3f2d2
CCSH <624089195@qq.com>
2018-08-25 10:31:22 +0800

    添加图片

--- SHAlertViewExmpale/SHAlertViewExmpale/ViewController.m
+++ SHAlertViewExmpale/SHAlertViewExmpale/ViewController.m
@@ -20,35 +20,44 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+- (IBAction)btn1Action:(id)sender {
     
-//    SHAlertView *ale = [[SHAlertView alloc]initWithTitle:@"标题" message:@"内容" cancelTitle:@"取消" sureTitle:@"确认" cancelAction:nil sureAction:^{
-//        NSLog(@"确认");
-//    }];
-//    [ale show];
+    SHAlertView *ale = [[SHAlertView alloc]initWithTitle:@"标题" message:@"内容" cancelTitle:@"取消" sureTitle:@"确认" cancelAction:nil sureAction:^{
+        NSLog(@"确认");
+    }];
+    [ale show];
+}
+
+- (IBAction)btn2Action:(id)sender {
     
-//    NSMutableParagraphStyle *style = [[NSMutableParagraphStyle alloc] init];
-//    style.lineSpacing = 4;
-//    style.alignment = NSTextAlignmentCenter;
-//
-//    NSDictionary *config = @{NSFontAttributeName:[UIFont systemFontOfSize:16],
-//                             NSForegroundColorAttributeName:[UIColor lightGrayColor],
-//                             NSParagraphStyleAttributeName:style};
-//    NSAttributedString *messageAtt = [[NSAttributedString alloc] initWithString:@"0987654321" attributes:config];
-//
-//    SHClickTextModel *model = [[SHClickTextModel alloc]init];
-//    model.parameter = @"参数1";
-//    model.range = NSMakeRange(1, 4);
-//
-//    SHAlertView *ale = [[SHAlertView alloc]initWithTitle:@"标题" messageAtt:messageAtt parameArr:@[model] cancelTitle:@"取消" sureTitle:@"确定" cancelAction:nil sureAction:nil textAction:^(NSString *parameter) {
-//        NSLog(@"%@",parameter);
-//    }];
-//    [ale show];
+    NSMutableParagraphStyle *style = [[NSMutableParagraphStyle alloc] init];
+    style.lineSpacing = 4;
+    style.alignment = NSTextAlignmentCenter;
+
+    NSDictionary *config = @{NSFontAttributeName:[UIFont systemFontOfSize:16],
+                             NSForegroundColorAttributeName:[UIColor lightGrayColor],
+                             NSParagraphStyleAttributeName:style};
+    NSAttributedString *messageAtt = [[NSAttributedString alloc] initWithString:@"0987654321" attributes:config];
+
+    SHClickTextModel *model = [[SHClickTextModel alloc]init];
+    model.parameter = @"参数1";
+    model.range = NSMakeRange(1, 4);
+
+    SHAlertView *ale = [[SHAlertView alloc]initWithTitle:@"标题" messageAtt:messageAtt parameArr:@[model] cancelTitle:@"取消" sureTitle:@"确定" cancelAction:nil sureAction:nil textAction:^(NSString *parameter) {
+        NSLog(@"%@",parameter);
+    }];
+    [ale show];
+}
+
+- (IBAction)btn3Action:(id)sender {
     
-//    SHAlertView *ale = [[SHAlertView alloc]initWithImage:[UIImage imageNamed:@"help_thank_note_inset"] cancelTitle:@"取消" sureTitle:@"确认" cancelAction:nil sureAction:^{
-//        NSLog(@"确认");
-//    }];
-//    [ale show];
+    SHAlertView *ale = [[SHAlertView alloc]initWithImage:[UIImage imageNamed:@"help_thank_note_inset"] cancelTitle:@"取消" sureTitle:@"确认" cancelAction:nil sureAction:^{
+        NSLog(@"确认");
+    }];
+    [ale show];
+}
+
+- (IBAction)btn4Action:(id)sender {
     
     SHAlertView *ale = [[SHAlertView alloc]initWithIcon:[UIImage imageNamed:@"help_thank_note_inset"] message:@"内容" cancelTitle:@"取消" sureTitle:@"确认" cancelAction:nil sureAction:^{
         NSLog(@"确认");
